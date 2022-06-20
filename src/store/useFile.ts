32c6206ff58752700284f6a5d2500da1094a86f5
Carlos Carral C <carloscarral13@gmail.com>
2022-06-20 18:05:34 -0500

    Make useFile typescript

---
+++ src/store/useFile.ts
@@ -0,0 +1,37 @@
+import { defineStore } from 'pinia';
+import { usePoolStore } from '../store/usePools';
+import { useWasm } from '../store/useWasm';
+
+export const useFileStore = defineStore('file', {
+    state: () => ({
+        arrayFiles: [],
+        fileContents: []
+    }),
+    getters: {},
+    actions: {
+        // agrega los archivos de la UoloadFile
+        addAllFiles(files) {
+            this.arrayFiles = files;
+        },
+        deleteFile(file: File) {
+            let index = this.arrayFiles.indexOf(file)
+            if (index > -1) {
+                this.arrayFiles.splice(index, 1)
+            }
+        },
+        async addFile(file: File) {
+            // TODO: Esta función debería de añadir un archivo a arrayFiles
+            const fileContents = await file.text();
+
+            let wasm = useWasm();
+            if (!wasm.wasmReady) {
+                await wasm.wasmInit();
+            }
+            // Esto sirve para validar que el formato del archivo es
+            // válido. Arroja excepción si no lo es.
+            wasm.initPools(fileContents);
+            this.arrayFiles.push(file);
+            this.fileContents.push(fileContents);
+        }
+    }
+});
