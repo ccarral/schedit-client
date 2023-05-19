4c54bc1faebbf40831d7207584656d1360ed3565
Carlos Carral [Vauxoo] <carlos.c@vauxoo.com>
2023-05-18 19:04:24 -0600

    [actions] add workflow for deploying to github pages

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     serve: {
         host: '0.0.0.0'
     },
-    // base: '/potro-horario-mirror/',
+    base: '/schedit-client/',
     build: {
         rollupOptions: {
             moduleContext: {
