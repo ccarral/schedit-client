ef2f2da688ede85bc68eca10a2621c9d02c29f71
Carlos Carral <carloscarral13@gmail.com>
2023-05-26 21:35:13 -0600

    Add conditional "Go to diagnostics" button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,6 +8,9 @@
             <router-link to="/" data-bs-toggle="tooltip" data-bs-placement="top" title="Inicio">
               <img src="../assets/logo-transparent.png" class="img-fluid" alt="fs" width="200">
             </router-link>
+            <router-link v-if="errorExists" :to="{ name: 'Diagnostics', query: $route.query }" class="btn btn-danger float-right mt-2 diagnostics-button">
+               Ir a diagnósticos 
+            </router-link>
           </div>
         </div>
       </div>
@@ -17,11 +20,29 @@
 </template>
 
 <script>
+import {mapState} from 'pinia';
+import {usePoolDiagnostics} from "../store/usePoolDiagnostics";
+
 export default {
-  name: 'Header'
+  name: 'Header',
+  computed:{
+    ...mapState(usePoolDiagnostics, ['diagnostics']),
+    errorExists(){
+        for (const [_file, diagnostic] of this.diagnostics){
+            if(!diagnostic.ok){
+                return true;
+            }
+        }
+        return false;
+    }
+  }
 }
 </script>
 
 <style>
-
+.diagnostics-button{
+    text-align: right;
+    float:right;
+    margin-left: 10vw;
+}
 </style>
